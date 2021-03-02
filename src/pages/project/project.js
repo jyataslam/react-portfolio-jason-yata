38577dfb04ab8611a03e13ab96a53ecb36ba43ac
Jason <jasonyata@gmail.com>
2021-03-02 08:27:33 -0800

    moved project data to json file instead of in state

--- src/pages/project/project.js
+++ src/pages/project/project.js
@@ -1,45 +1,9 @@
 import React, { Component, useEffect } from "react";
 import "./project.scss";
+import PROJECT_JSON from "../../assets/data/projects.json";
 
 class ProjectPage extends Component {
-    state = {
-        ocps: {
-            id: 1,
-            title: "Orange County Plastic Surgery",
-            routeName: "ocps",
-            bio:
-                "lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data lorem ipsum placeholder text dummy data ",
-            deliverables: [
-                {
-                    title: "Web Development",
-                },
-                {
-                    title: "Web Development",
-                },
-                {
-                    title: "Web Development",
-                },
-                {
-                    title: "Web Development",
-                },
-                {
-                    title: "Web Development",
-                },
-                {
-                    title: "Web Development",
-                },
-                {
-                    title: "Web Development",
-                },
-            ],
-            imageOne: "image_here",
-            imageTwo: "image_here",
-            imageThree: "image_here",
-            imageFour: "image_here",
-            imageFive: "image_here",
-            imageSix: "image_here",
-        },
-    };
+    state = PROJECT_JSON;
 
     render() {
         const name = this.props.match.params.projectName;
@@ -54,6 +18,10 @@ class ProjectPage extends Component {
             imageFive,
             imageSix,
         } = this.state[name];
+        console.log(
+            "deliverables",
+            deliverables.map((item, index) => console.log(item.title))
+        );
 
         return (
             <div className="project">
