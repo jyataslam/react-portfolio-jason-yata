59c721b9af8752f00933682ea07cf8cef9b93365
Jason <jasonyata@gmail.com>
2021-03-02 20:24:25 -0800

    about page initial build complete

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.scss";
-import logo from "../../assets/images/jason-yata-logo-min.png";
+import logo from "../../assets/images/logo.png";
 
 const Header = () => {
     return (
--- src/pages/about/aboutpage.js
+++ src/pages/about/aboutpage.js
@@ -1,7 +1,48 @@
 import React from "react";
+import headshot from "../../assets/images/headshot.jpg";
+import "./aboutpage.scss";
 
 const AboutPage = () => {
-    return <div className="about">about page loaded</div>;
+    return (
+        <section className="about">
+            <div className="image-container">
+                <img
+                    src={headshot}
+                    alt="jason yata web development and design headshot"
+                />
+            </div>
+            <div className="about-container">
+                <p className="location">orange county | california</p>
+                <h1 className="name">Jason Yata</h1>
+                <p className="title">
+                    Web Developer. Software Engineer. Creative Designer.
+                </p>
+                <p className="bio">
+                    I work as a web developer and software engineer creating
+                    websites and applications for companies. I currently work
+                    professionally with Javascript, React, Redux, Gatsby,
+                    GraphQL, and many other technologies.
+                </p>
+                <p>
+                    In my free time I co-host a web dev podcast
+                    <a
+                        href="https://open.spotify.com/show/3CLctEf29mQHmosLDDFSst?si=CaohK_0tR063v5MVqH4pjg"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        @badforeducation
+                    </a>
+                    <br></br>I also love running, traveling, and listening to
+                    audiobooks.
+                </p>
+                <div className="icon-container">
+                    <i className="fa fa-spotify"></i>
+                    <i className="fa fa-linkedin"></i>
+                    <i className="fa fa-instagram"></i>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default AboutPage;
