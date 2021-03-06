5154873eab35e04b77dd27b52a5c73ee74451a45
Jason <jasonyata@gmail.com>
2021-03-05 16:16:41 -0800

    removed unused vars in blog-single-page.js

--- src/components/blog-single-info/blog-single-info.js
+++ src/components/blog-single-info/blog-single-info.js
@@ -3,13 +3,6 @@ import "./blog-single-info.scss";
 
 const BlogSingleInfo = ({ info }) => {
     const { content, date, featuredImg, images, title } = info;
-    const ppOne = content[0].text;
-    const ppTwo = content[1].text;
-    const ppThree = content[2].text;
-    // const ppFour = content[3].text;
-    const img1 = images[0].url;
-    const img2 = images[1].url;
-    // const img3 = images[3].url;
     return (
         <section className="blog-single-info">
             <div className="hero">
